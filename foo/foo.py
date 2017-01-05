360b12915a4d20483ea62ea230c00e790d92f992
bluethon <j5088794@gmail.com>
2017-01-05 18:34:39 +0800

    ch1 hello finished

--- foo/foo.py
+++ foo/foo.py
@@ -11,7 +11,7 @@ from django.conf import settings
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 
-SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+SECRET_KEY = os.environ.get('SECRET_KEY', '(%6$mh10^@lpcnl9-c#be1esy87yu$_94#4@r@jr-agz30%6o#')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
@@ -40,9 +40,7 @@ urlpatterns = (
     url(r'^$', index),
 )
 
-
 application = get_wsgi_application()
-
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
---
+++ note.md
@@ -0,0 +1,19 @@
+Cmd Note
+=========
+
+### Set envirn
+
+    export DEBUG=on
+    unset DEBUG
+
+### use the template with `startproject`
+
+使用自定义模板生成新项目
+
+**注意模板使用{{ secret_key }}**
+
+    django-admin.py startproject foo --template=project_name
+
+### use Gunicorn and set output to console
+
+    gunicorn hello --log-file=-
