0fcb4d4b6ad576873c50dc0186ed2817213f5ae4
bluethon <j5088794@gmail.com>
2017-01-06 17:20:32 +0800

    ch2 image create

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -27,9 +27,46 @@ settings.configure(
     ),
 )
 
+from django import forms
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image."""
+
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+
+    def generate(self, image_format='PNG'):
+        """Generate an image of the given type and return as raw bytes."""
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image = Image.new('RGB', (width, height))
+        draw = ImageDraw.Draw(image)
+        text = '{} X {}'.format(width, height)
+        textwidth, textheight = draw.textsize(text)
+        if textwidth < width and textheight < height:
+            texttop = (height - textheight) // 2
+            textleft = (width - textwidth) // 2
+            draw.text((textleft, texttop), text, fill=(255, 255, 255))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
+
+
+def placeholder(request, width, height):
+    form = ImageForm({'height': height, 'width': width, })
+    if form.is_valid():
+        image = form.generate()
+        return HttpResponse(image, content_type='image/png')
+    else:
+        return HttpResponseBadRequest('Invalid Image Request')
 
 
 def index(request):
@@ -37,12 +74,11 @@ def index(request):
 
 
 urlpatterns = (
-    url(r'^$', index),
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
+    url(r'^$', index, name='homepage'),
 )
 
-
 application = get_wsgi_application()
-
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
