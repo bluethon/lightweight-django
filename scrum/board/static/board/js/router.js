9155c7145345967db821dc3d3f69ef66c94da237
bluethon <j5088794@gmail.com>
2017-08-28 00:23:38 +0800

    ch5 Creating a Header View - to debug

--- scrum/board/static/board/js/router.js
+++ scrum/board/static/board/js/router.js
@@ -7,14 +7,53 @@
         initialize: function (options) {
             this.contentElement = '#content';
             this.current = null;
+            this.header = new app.views.HeaderView();
+            // added to the start of the <body>
+            $('body').prepend(this.header.el);
+            this.header.render();
             Backbone.history.start();
         },
         home: function () {
             var view = new app.views.HomepageView({el: this.contentElement});
             this.render(view);
         },
+        route: function (route, name, callback) {
+            // Override default route to enforce login on every page
+            var login;
+            callback = callback || this[name];
+            // _.wrap(function, wrapper)
+            // 将第一个函数 function 封装到函数wrapper里面, 并把函数 function 作为第一个参数传给wrapper.
+            // 这样可以让 wrapper 在 function 运行之前和之后 执行代码, 调整参数然后附有条件地执行.
+            callback = _.wrap(callback, function (original) {
+                // _.without(array, *values)
+                // 返回一个删除所有values值后的array副本
+                // TODO: arguments?
+                var args = _.without(arguments, original);
+                if (app.session.authenticated()) {
+                    original.apply(this, args);
+                } else {
+                    // Show the login screen before calling the view
+                    $(this.contentElement).hide();
+                    // Bind original callback once the login is successful
+                    login = new app.views.LoginView();
+                    $(this.contentElement).after(login.el);
+                    // 触发done事件时, 执行函数
+                    login.on('done', function () {
+                        // When logout is finished, the header is rendered again to reflect the new state
+                        this.header.render();
+                        $(this.contentElement).show();
+                        original.apply(this, args);
+                    }, this);
+                    // Render the login form
+                    login.render();
+                }
+            });
+            // 原始的route调用时会执行修饰过(wrapped)的callback函数
+            return Backbone.Router.prototype.route.apply(this, [route, name, callback]);
+        },
         render: function (view) {
             if (this.current) {
+                this.current.undelegateEvents();
                 this.current.$el = $();
                 this.current.remove();
             }
--- scrum/board/static/board/js/views.js
+++ scrum/board/static/board/js/views.js
@@ -1,6 +1,6 @@
 (function ($, Backbone, _, app) {
 
-    var TemplateView =  Backbone.View.extend({
+    var TemplateView = Backbone.View.extend({
         templateName: '',
         initialize: function () {
             this.template = _.template($(this.templateName).html());
@@ -15,17 +15,39 @@
         }
     });
 
-    var HomepageView = TemplateView.extend({
-        templateName: '#home-template'
-    });
-
-    var LoginView = TemplateView.extend({
-        id: 'login',
-        templateName: '#login-template',
-        errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+    var FormView = TemplateView.extend({
         events: {
             // 监听(listen)所有 form的submit, 执行submit回调(callback)
             'submit form': 'submit'
+        },
+        errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+        clearErrors: function () {
+            $('.error', this.form.remove());
+        },
+        showErrors: function (errors) {
+            // TODO: Show the errors from the response
+            // 映射对象obj,         obj.v         obj.k
+            _.map(errors, function (fieldErrors, name) {
+                var field = $(':input[name=' + name + ']', this.form),
+                    label = $('label[for=' + field.attr('id') + ']', this.form);
+                if (label.length === 0) {
+                    label = $('label', this.form).first();
+                }
+
+                function appendError(msg) {
+                    label.before(this.errorTemplate({msg: msg}));
+                }
+
+                _.map(fieldErrors, appendError, this);      // 循环增加显示的错误html
+                // errors本身(context)
+            }, this);
+        },
+        serializeForm: function (form) {
+            // _.object, 对象化, 即{}
+            return _.object(_.map(form.serializeArray(), function (item) {
+                // Convert object to tuple of (name, value)
+                return [item.name, item.value];
+            }));
         },
         submit: function (event) {
             var data = {};
@@ -33,40 +55,82 @@
             event.preventDefault();
             this.form = $(event.currentTarget);
             this.clearErrors();
-            data = {
-                username: $(':input[name="username"]', this.form).val(),
-                password: $(':input[name="password"]', this.form).val()
-            };
-            // TODO: Submit the login form
-            $.post(app.apiLogin, data)
-                .success($.proxy(this.loginSuccess, this))
-                .fail($.proxy(this.loginFailure, this));
         },
-        loginSuccess: function (data) {
-            app.session.save(data.token);
-            this.trigger('login', data.token);
-        },
-        loginFailure: function (xhr, status, error) {
+        failure: function (xhr, status, error) {
             var errors = xhr.responseJSON;
             this.showErrors(errors);
         },
-        showErrors: function (errors) {
-            // TODO: Show the errors from the response
-            _.map(errors, function (fieldErrors, name) {
-                var field = $(':inuput[name=' + name + ']', this.form),
-                    label = $('label[for=' + field.attr('id') + ']', this.form);
-                if (label.length === 0) {
-                    label = $('label', this.form).first();
-                }
-                function appendError(msg) {
-                    label.before(this.errorTemplate({msg: msg}));
-                }
-                _.map(fieldErrors, appendError, this);
-            }, this);
-        },
-        clearErrors: function () {
-            $('.error', this.form).remove();
+        done: function (event) {
+            if (event) {
+                event.preventDefault();
+            }
+            // trigger event
+            this.trigger('done');
+            this.remove();
         }
+    });
+
+    var HomepageView = TemplateView.extend({
+        templateName: '#home-template'
+    });
+
+    var LoginView = TemplateView.extend({
+        id: 'login',
+        templateName: '#login-template',
+        // errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+        // events: {
+        //     // 监听(listen)所有 form的submit, 执行submit回调(callback)
+        //     'submit form': 'submit'
+        // },
+        submit: function (event) {
+            var data = {};
+
+            // // 阻止默认提交, 由view来处理提交和返回
+            // event.preventDefault();
+            // this.form = $(event.currentTarget);
+            // this.clearErrors();
+            // data = {
+            //     username: $(':input[name="username"]', this.form).val(),
+            //     password: $(':input[name="password"]', this.form).val()
+            // };
+            // js没有super, 但是下面语句等价调用了父方法
+            FormView.prototype.submit.apply(this, arguments);
+            data = this.serializeForm(this.form);
+
+            $.post(app.apiLogin, data)
+            // $.proxy(), 接受一个函数，然后返回一个新函数，并且这个新函数始终保持了特定的上下文语境
+                .success($.proxy(this.loginSuccess, this))
+                .fail($.proxy(this.failure, this));
+        },
+        loginSuccess: function (data) {
+            app.session.save(data.token);
+            // this.trigger('login', data.token);
+            this.done();
+        }
+        // loginFailure: function (xhr, status, error) {
+        //     var errors = xhr.responseJSON;
+        //     this.showErrors(errors);
+        // },
+        // showErrors: function (errors) {
+        //     // TODO: Show the errors from the response
+        //     // 映射对象obj,         obj.v         obj.k
+        //     _.map(errors, function (fieldErrors, name) {
+        //         var field = $(':input[name=' + name + ']', this.form),
+        //             label = $('label[for=' + field.attr('id') + ']', this.form);
+        //         if (label.length === 0) {
+        //             label = $('label', this.form).first();
+        //         }
+        //         function appendError(msg) {
+        //             label.before(this.errorTemplate({msg: msg}));
+        //         }
+        //         _.map(fieldErrors, appendError, this);      // 循环增加显示的错误html
+        //     // errors本身(context)
+        //     }, this);
+        // },
+        // // 移除form中所有存在的class="error"的元素
+        // clearErrors: function () {
+        //     $('.error', this.form).remove();
+        // }
     });
 
     // var HomepageView = Backbone.View.extend({
@@ -99,8 +163,29 @@
     //         return {};
     //     }
     // });
+    var HeaderView = TemplateView.extend({
+        // this means the template renders into a <header> element
+        tagName: 'header',
+        templateName: '#header-template',
+        // TODO: ?, 包括书中解释, two link?
+        events: {
+            'click a.logout': 'logout'
+        },
+        // The authenticated value is passed to the template context based on the current session state
+        // It won't automatically be updated if this state changes
+        // The view will have to be rendered again
+        getContext: function () {
+            return {authenticated: app.session.authenticated()};
+        },
+        logout: function (event) {
+            event.preventDefault();
+            app.session.delete();
+            window.location = '/';
+        }
+    });
 
     app.views.HomepageView = HomepageView;
     app.views.LoginView = LoginView;
+    app.views.HeaderView = HeaderView;
 
 })(jQuery, Backbone, _, app);
